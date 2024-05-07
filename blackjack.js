ac36f9710a61669627c907f6187df9685d202eb4
Jesper <jesper.drotz@skola.taby.se>
2024-05-07 14:22:06 +0200

    yup

--- blackjack.js
+++ blackjack.js
@@ -1,130 +1,27 @@
-let canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth - 10;
-canvas.height = window.innerHeight - 10;
+var dealersum = 0;
+var playersum = 0;
 
-let context = canvas.getContext("2d")
+var dealeracecount =0;//håller koll på hur många ace dealern har 
+var youracecount = 0; //håller koll på hur många ace playern har 
 
+var hidden;
+var Deck;
 
-var used_cards = new Array();
+var canhit = true; // så att du kan hit
 
-function card(name,suit,value) {
-    this.name = name;
-    this.suit = suit;
-    this.value = value;
-} 
+window.onload = function(){
+    builddeck();
+}
 
+function builddeck(){
+    let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    let types = ["C","D","H","S"];
+    deck = [];
 
-var deck = [
-    new card('Ace', 'Hearts',11),
-    new card('Two', 'Hearts',2),
-    new card('Three', 'Hearts',3),
-    new card('Four', 'Hearts',4),
-    new card('Five', 'Hearts',5),
-    new card('Six', 'Hearts',6),
-    new card('Seven', 'Hearts',7),
-    new card('Eight', 'Hearts',8),
-    new card('Nine', 'Hearts',9),
-    new card('Ten', 'Hearts',10),
-    new card('Jack', 'Hearts',10),
-    new card('Queen', 'Hearts',10),
-    new card('King', 'Hearts',10),
-    new card('Ace', 'Diamonds',11),
-    new card('Two', 'Diamonds',2),
-    new card('Three', 'Diamonds',3),
-    new card('Four', 'Diamonds',4),
-    new card('Five', 'Diamonds',5),
-    new card('Six', 'Diamonds',6),
-    new card('Seven', 'Diamonds',7),
-    new card('Eight', 'Diamonds',8),
-    new card('Nine', 'Diamonds',9),
-    new card('Ten', 'Diamonds',10),
-    new card('Jack', 'Diamonds',10),
-    new card('Queen', 'Diamonds',10),
-    new card('King', 'Diamonds',10),
-    new card('Ace', 'Clubs',11),
-    new card('Two', 'Clubs',2),
-    new card('Three', 'Clubs',3),
-    new card('Four', 'Clubs',4),
-    new card('Five', 'Clubs',5),
-    new card('Six', 'Clubs',6),
-    new card('Seven', 'Clubs',7),
-    new card('Eight', 'Clubs',8),
-    new card('Nine', 'Clubs',9),
-    new card('Ten', 'Clubs',10),
-    new card('Jack', 'Clubs',10),
-    new card('Queen', 'Clubs',10),
-    new card('King', 'Clubs',10),
-    new card('Ace', 'Spades',11),
-    new card('Two', 'Spades',2),
-    new card('Three', 'Spades',3),
-    new card('Four', 'Spades',4),
-    new card('Five', 'Spades',5),
-    new card('Six', 'Spades',6),
-    new card('Seven', 'Spades',7),
-    new card('Eight', 'Spades',8),
-    new card('Nine', 'Spades',9),
-    new card('Ten', 'Spades',10),
-    new card('Jack', 'Spades',10),
-    new card('Queen', 'Spades',10),
-    new card('King', 'Spades',10)
-];
-
-var hand = {
-    cards : new Array(),
-    current_total : 0,
-
-    sumcardtotal:function(){
-        this. current_total=0;
-        for (let i = 0; i< this.cards.length; i++) {
-          const c = this.cards[i];
-          this.current_total += c.value;
-           
+    for (let i = 0; i < types.length; i++) {
+        for (let x = 0; x < values.length; x++) {
+            deck.push(values[x] + "-" + types[i]);
         }
-        if (this.current_total> 21){
-            console.log("BUST!!!!!!!!!!!!")
-            }
-        else if(this.current_total ===21){
-                console.log("BLACKJACK");
-            }
-            else{
-
-            }
-
     }
-}
-
-function getrandomnum(num){
-    var my_num = Math.floor(Math.random()*num);
-    return my_num
-}
-
-function deal(){
-    for (let i = 0; i < 2; i++){
-        hit()
-    }
-}
-function hit(){
-    var good_card = false;
-    do {
-        var index = getrandomnum(52);
-        if(!$.inArray(index.used_cards) > -1){
-            good_card = true;
-            var c = [index];
-            used_cards[used_cards.length] = index;
-            hand.cards [hand.cards.length] = c;
-        }
-    } while(good_card == false)
-}
-
-
-
-function clearCanvas() {
-    context.fillStyle = "white"
-    context.fillRect(0,0,canvas.width,canvas.height)
-
-}
-
-function update () {
-    clearCanvas()
 }
 
